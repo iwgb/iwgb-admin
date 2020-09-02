3d416cab245fd4b68b6c1da05c185183372a9517
Guy Mac <hello@guymac.eu>
2020-09-02 12:59:39 +0100

    :sparkles: loading state

--- src/components/SorterResultList/SorterResultList.jsx
+++ src/components/SorterResultList/SorterResultList.jsx
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useQuery } from '@apollo/react-hooks';
+import Skeleton, { SkeletonTheme } from 'react-loading-skeleton';
+import { toast } from 'react-toastify';
 import SorterResults from '../../gql/sorterResults';
 import JobTypes from '../../service/jobTypes.service';
 import Plans from '../../service/plans.service';
@@ -18,24 +20,31 @@ const SorterResultList = () => {
 
   const { loading, error, data: { sorterResults = [] } = {} } = useQuery(SorterResults.getAll);
 
-  if (loading) {
-    return 'Loading';
-  }
   if (error) {
-    return 'Error';
+    toast('Could not load data');
   }
 
   return (
     <div>
-      { sorterResults.map((sorterResult) => (
-        <span key={sorterResult.identifier}>
-          <SorterResult
-            sorterResult={sorterResult}
-            jobTypes={jobTypes}
-            plans={plans}
-          />
-        </span>
-      ))}
+      {
+        loading
+          ? (
+            <SkeletonTheme color="#f7f7f7">
+              {[...new Array(15)].fill().map(() => (
+                <Skeleton height="4rem" className="my-1" />
+              ))}
+            </SkeletonTheme>
+          )
+          : sorterResults.map((sorterResult) => (
+            <span key={sorterResult.identifier}>
+              <SorterResult
+                sorterResult={sorterResult}
+                jobTypes={jobTypes}
+                plans={plans}
+              />
+            </span>
+          ))
+      }
     </div>
   );
 };
