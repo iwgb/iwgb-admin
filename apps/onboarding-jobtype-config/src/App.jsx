6fea49e4ff630dc001ac56e251473f842a0e08fa
Guy Mac <hello@guymac.eu>
2020-09-15 00:46:41 +0100

    :sparkles: finishing touches

--- apps/onboarding-jobtype-config/src/App.jsx
+++ apps/onboarding-jobtype-config/src/App.jsx
@@ -1,14 +1,25 @@
 import React from 'react';
-import './App.scss';
 import { ApolloProvider } from '@apollo/react-hooks';
 import { ToastContainer } from 'react-toastify';
 import { RecoilRoot } from 'recoil';
+import { Helmet } from 'react-helmet';
 import client from './helpers/apollo';
 import SorterResultList from './components/SorterResultList';
 import 'react-toastify/dist/ReactToastify.css';
+import './App.scss';
 
 const App = () => (
   <div className="container">
+    <Helmet>
+      <title>Applicant sorter config · Admin · IWGB</title>
+      {[32, 64, 96, 128].map((size) => (
+        <link
+          rel="icon"
+          href={`https://cdn.iwgb.org.uk/assets/favicon-${size}.png`}
+          sizes={`${size}x${size}`}
+        />
+      ))}
+    </Helmet>
     <ApolloProvider client={client}>
       <RecoilRoot>
         <SorterResultList />
